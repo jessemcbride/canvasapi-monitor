4c52015919da73afc7eb06b65fd91264642bc353
Jesse McBride <jm@ucf.edu>
2017-11-27 16:58:05 -0500

    Add giant try-except to debug server.

--- monitor.py
+++ monitor.py
@@ -16,40 +16,42 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
 methods = soup.find_all('div', {'class': 'method_details'})
+try:
+    for method in methods:
+        try:
+            name = method.find_all('h2', {'class': 'api_method_name'})[0].find_all('a')[0].text.strip()
+            identifier = method.find_all('h2', {'class': 'api_method_name'})[0]['name']
+            method_url = url + method.find_all('a', href=True)[0]['href']
+        except IndexError:
+            print("Couldn't find method name for method:\n%s" % method)
 
-for method in methods:
-    try:
-        name = method.find_all('h2', {'class': 'api_method_name'})[0].find_all('a')[0].text.strip()
-        identifier = method.find_all('h2', {'class': 'api_method_name'})[0]['name']
-        method_url = url + method.find_all('a', href=True)[0]['href']
-    except IndexError:
-        print("Couldn't find method name for method:\n%s" % method)
+        try:
+            cached = open(path + '/cache/%s' % identifier.replace('/', '+'), 'r+')
 
-    try:
-        cached = open(path + '/cache/%s' % identifier.replace('/', '+'), 'r+')
+            if cached.read() != str(method):
+                changes.append({'method': name, 'url': method_url})
 
-        if cached.read() != str(method):
+            cached.seek(0)
+            cached.truncate()
+            cached.write(str(method))
+            cached.close()
+        except IOError:
+            cached = open(path + '/cache/%s' % identifier.replace('/', '+'), 'w+')
+            cached.write(str(method))
+            cached.close()
             changes.append({'method': name, 'url': method_url})
 
-        cached.seek(0)
-        cached.truncate()
-        cached.write(str(method))
-        cached.close()
-    except IOError:
-        cached = open(path + '/cache/%s' % identifier.replace('/', '+'), 'w+')
-        cached.write(str(method))
-        cached.close()
-        changes.append({'method': name, 'url': method_url})
+    if changes:
+        print('Discovered %d changes' % len(changes))
+        plural = 's' if len(changes) > 1 else ''
 
-if changes:
-    print('Discovered %d changes' % len(changes))
-    plural = 's' if len(changes) > 1 else ''
+        message = "Detected modified endpoint" + plural + ":\n"
 
-    message = "Detected modified endpoint" + plural + ":\n"
+        for change in changes:
+            message += '\n<%s|%s>' % (change['url'], change['method'])
 
-    for change in changes:
-        message += '\n<%s|%s>' % (change['url'], change['method'])
-
-    slack.notify(text=message)
-else:
-    print('No changes found')
+        slack.notify(text=message)
+    else:
+        print('No changes found')
+except Exception as e:
+    print e
